5ce3c88ba8af10f4fc86d2f4bc9579b79255635b
mrfriday05 <domonkos.pentek@gmail.com>
2025-11-10 13:49:39 +0100

    Co-authored-by: robycsomo <robycsomo@users.noreply.github.com>

--- ADCS_simulation.py
+++ ADCS_simulation.py
@@ -11,10 +11,10 @@ class SatSym ():
     reflectivity:float
     c:float
 
-    def __init__(self, MoI, cp_to_cm, A_max, solar_constant, reflectivity, c, theta_eclipse, drag_force):
+    def __init__(self, MoI, cp_to_cm, A_max, solar_constant, reflectivity, c, theta_eclipse, drag_force,gravity_torque,gravity_direction, rw_inaccuracy):
         self.MoI=MoI
         self.cp_to_cm=cp_to_cm
-        self.phi0=np.arctan(cp_to_cm[0]/cp_to_cm[2])
+        self.phi0=np.arctan(cp_to_cm[0]/-cp_to_cm[2])
         self.cp_to_cm_dist=np.linalg.norm(cp_to_cm)
         self.A_max=A_max
         self.solar_constant=solar_constant
@@ -22,31 +22,41 @@ class SatSym ():
         self.c=c
         self.theta_eclipse=theta_eclipse
         self.drag_force=drag_force
-        self.drag_torque=[0,drag_force*self.cp_to_cm*self.cp_to_cm[1],0]
+        self.drag_torque_magnitude=-drag_force*self.cp_to_cm[2]
+        self.gravity_torque_magnitude=gravity_torque
+        self.gravity_direction=gravity_direction
+        self.rw_inaccuracy=rw_inaccuracy
+        
 
     def solar_torque(self, theta):
         if theta>self.theta_eclipse or theta< -self.theta_eclipse:
             return np.array([0,0,0])
-        T_y=self.solar_constant/self.c*self.A_max*(1+self.reflectivity)*np.cos(theta-self.phi0)*self.cp_to_cm
+        T_y=self.solar_constant/self.c*self.A_max*(1+self.reflectivity)*np.cos(theta-self.phi0)*self.cp_to_cm_dist
+        
         return np.array([0,T_y,0])
     
     def gravity_torque(self):
-        return np.array([0,0,0])
-    
+        return self.gravity_torque_magnitude*self.gravity_direction
+        
     def magnetic_torque(self,theta):
         return np.array([0,0,0])
     
-    def actuator_torque(self,theta, command):
-        return np.array([0,0,0])
+    def drag_torque(self):
+        return np.array([0,self.drag_torque_magnitude,0])
     
-    def actuator_innacuracy_torque(self,theta):
-        return np.array([0,0,0])
+    def rw_torque(self, rw_command):
+        rw_t=rw_command*(1+2*self.rw_inaccuracy*np.random.rand(1)[0]-self.rw_inaccuracy)
+        return rw_t
     
-    def total_torque(self,theta):
-        return self.solar_torque(theta) + self.gravity_torque(theta) + self.magnetic_torque(theta) + self.drag_torque + self.actuator_innacuracy_torque(theta)   
+    def thruster_torque(self, t_command):
+        t_t=t_command*(1+2*self.rw_inaccuracy*np.random.rand(1)[0]-self.rw_inaccuracy)
+        return t_t
 
-    def step(self, angvec, omega, dt):
-        alpha=np.linalg.inv(self.MoI)@self.total_torque(angvec)-np.cross(omega, self.MoI@omega)
+    def total_torque(self,theta, rw_command, t_command):
+        return self.solar_torque(theta) + self.gravity_torque() + self.magnetic_torque(theta) + self.drag_torque() + self.rw_torque(rw_command) + self.thruster_torque(t_command)
+
+    def step(self, true_anomaly, angvec, omega, dt, rw_command, t_command):
+        alpha=np.linalg.inv(self.MoI)@self.total_torque(true_anomaly, rw_command, t_command)-np.cross(omega, self.MoI@omega)
         omega+=alpha*dt
-        angvec+=omega[1]*dt
-        return angvec, omega
+        angvec+=omega*dt
+        return angvec, omega, alpha
--- main.py
+++ main.py
@@ -3,31 +3,49 @@ import matplotlib.pyplot as plt
 from ADCS_simulation import SatSym
 
 Simulator = SatSym(                         MoI=np.diag([432.2,237.8,495.1]),
-                                            cp_to_cm=np.array([0.018,0.0,0.011]),
+                                            cp_to_cm=np.array([-0.04,0.0,0.03]), #np.array([-0.018,0.0,0.011]),
                                             A_max=1.5**2*np.sqrt(2),
                                             solar_constant=2601,
                                             reflectivity=0.2,
                                             c=3e8,
                                             theta_eclipse=np.radians(139),
-                                            drag_force=2.319e-4 
+                                            drag_force=2.319*10**-5,
+                                            gravity_direction=np.array([0.58018, -0.593139, 0.558192]),
+                                            gravity_torque=3.308*(10**-6),
+                                            rw_inaccuracy=0.05
                                             )
-dt=1
-T=1000
-omega=np.array([0.0,0.0,0.0])
+dt=0.3
+simulated_time=6000 
+T_orbit=6530
+orbital_angular_velocity=2*np.pi/T_orbit                                                    
+true_anomaly=0.0                                #true anomaly
+omega=np.array([0.0,0.0,0.0])       #angular velocity
+angvec=np.array([0.0,0.0,0.0])                  #angles between nadir and -w axis
 ttab=[]
 
-thetatab=[]
-omegatab=[]
+pointingtab=[]
+omegatab=[] 
+alphatab=[]
 
 
-for i in range (int(T/dt)):
-    
-    Simulator.step(Simulator.theta, omega, dt)
+
+for i in range (int(simulated_time/dt)):
+    true_anomaly += orbital_angular_velocity*dt
+    if true_anomaly > np.pi:
+        true_anomaly -= 2*np.pi
+    rw_c=np.array([0.0,0.0,0.0])
+    t_c=np.array([0.0,0.0,0.0])
+
     ttab.append(i*dt)
-    thetatab.append(Simulator.theta)
-    omegatab.append(Simulator.omega)
+    angvec, omega, alpha=Simulator.step(true_anomaly=true_anomaly, omega=omega, angvec=angvec, dt=dt, rw_command=rw_c, t_command=t_c)
+    
+    pointingtab.append(angvec.copy())
+    omegatab.append(omega.copy())
+    alphatab.append(alpha.copy())
 
-plt.plot(ttab, thetatab)
+plt.plot(ttab, np.array(omegatab)[:,1])
+plt.plot(ttab, np.array(alphatab)[:,1]*1000)
+plt.plot(ttab, np.array(pointingtab)[:,1]*100000)
 plt.xlabel('Time (s)')
 plt.ylabel('Angle (rad)')
 plt.title('Angle vs Time')
